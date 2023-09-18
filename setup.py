48d516cb6e6210561142bc31576eab6196eac799
craig.warner <cgwarner2014@gmail.com>
2023-09-17 22:36:43 -0500

    trials

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="twister-spinner", #
+    name="twister-spinner", 
     version="1.0.0",
     author="Craig Warner",
     author_email="cgwarner2014@gmail.com",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    #python_requires='>=3.6',
     install_requires=['argparse>=1.4.0'],
     scripts=['bin/twister-spinner',
             "bin/version.py"
